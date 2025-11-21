4a43ca7bd54bd4b2788527c251df8cd081ed50d6
lsu123 <lsu123@gmail.com>
2025-11-21 11:51:50 -0500

    added asteroids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,14 +13,25 @@ def main():
     print('Starting Asteroids with pygame version: (%s)' % pygame.version.ver)
     print('Screen width: %s' % SCREEN_WIDTH)
     print('Screen height: %s' % SCREEN_HEIGHT)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("red")
-        player.draw(screen)
+        screen.fill("black")
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000  # 60 FPS cap + catch delta time and convert to seconds
 
